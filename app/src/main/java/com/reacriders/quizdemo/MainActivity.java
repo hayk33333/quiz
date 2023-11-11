7b067b4e6d905c81b3a8f6404df71de42fe8eaba
unset <unset>
2023-11-11 17:13:45 +0400

    first commit

---
+++ app/src/main/java/com/reacriders/quizdemo/MainActivity.java
@@ -0,0 +1,99 @@
+package com.reacriders.quizdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView totalQuestionsTextView, questionTextView;
+    Button ansAButton;
+    Button ansBButton;
+    Button ansCButton;
+    Button ansDButton;
+    Button oKButton;
+    int totalQuestions = QuestionAnswers.question.length;
+    String selectedAnswer = "";
+    int currentQuestionIndex = 0;
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansAButton = findViewById(R.id.ans_a);
+        ansBButton = findViewById(R.id.ans_b);
+        ansCButton = findViewById(R.id.ans_c);
+        ansDButton = findViewById(R.id.ans_d);
+        oKButton = findViewById(R.id.ok_button);
+
+        // Set up click listeners
+        ansAButton.setOnClickListener(this);
+        ansBButton.setOnClickListener(this);
+        ansCButton.setOnClickListener(this);
+        ansDButton.setOnClickListener(this);
+        oKButton.setOnClickListener(this);
+        loadNewQuestion();
+    }
+
+    @Override
+    public void onClick(View view) {
+        ansAButton.setBackgroundColor(Color.WHITE);
+        ansBButton.setBackgroundColor(Color.WHITE);
+        ansCButton.setBackgroundColor(Color.WHITE);
+        ansDButton.setBackgroundColor(Color.WHITE);
+
+        Button button = (Button) view;
+        if (button.getId() == R.id.ok_button) {
+            if (selectedAnswer.equals(QuestionAnswers.answers[currentQuestionIndex])) {
+                score++;
+            }
+
+            currentQuestionIndex++;
+            loadNewQuestion();
+        } else {
+            selectedAnswer = button.getText().toString();
+            button.setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    private void loadNewQuestion() {
+        if (currentQuestionIndex == totalQuestions){
+            finishQuiz();
+            return;
+        }
+        questionTextView.setText(QuestionAnswers.question[currentQuestionIndex]);
+        ansAButton.setText(QuestionAnswers.choices[currentQuestionIndex][0]);
+        ansBButton.setText(QuestionAnswers.choices[currentQuestionIndex][1]);
+        ansCButton.setText(QuestionAnswers.choices[currentQuestionIndex][2]);
+        ansDButton.setText(QuestionAnswers.choices[currentQuestionIndex][3]);
+    }
+
+    private void finishQuiz() {
+        String passStatus = "";
+        if (score > totalQuestions * 0.6){
+            passStatus = "Passed";
+        }else{
+            passStatus = "failed";
+        }
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is " + score + " out of " + totalQuestions)
+                .setPositiveButton("restart", ((dialogInterface, i) -> restartQuiz()))
+                .setCancelable(false)
+                .show();
+    }
+
+    private void restartQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        loadNewQuestion();
+    }
+}
---
+++ app/src/main/java/com/reacriders/quizdemo/QuestionAnswers.java
@@ -0,0 +1,25 @@
+package com.reacriders.quizdemo;
+
+public class QuestionAnswers {
+    public static String question[] = {
+            "how much is 1 + 1?",
+            "how much is 2 + 2?",
+            "how much is 3 + 3?",
+    };
+
+    public static String choices[][] = {
+            {"1", "2", "3", "4" },
+            {"2", "4", "6", "8" },
+            {"3", "6", "9", "12" },
+
+    };
+
+    public static String answers[] = {
+            "2",
+            "4",
+            "6",
+
+    };
+
+
+}
